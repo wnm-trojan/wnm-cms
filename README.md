3da68f745f05b10dd552905571be12c73e9885ec
wnm-trojan <warunanissanka44@gmail.com>
2023-11-26 22:32:17 +0530

    Project initializes

---
+++ README.md
@@ -0,0 +1,8 @@
+# WNM CMS
+This is a Laravel base CMS
+
+## Frontend
+Frontend has been developed using <a href="https://laravel.com/docs/10.x/blade">Blade</a>. Frontend authentication flow using <a href="https://laravel.com/docs/10.x/blade">Blade</a> and <a href="https://tailwindcss.com/">Tailwind</a> so that you can simply start building your next big idea. Use <a href="https://alpinejs.dev/">Alpine.js</a> for composing behavior directly in your markup.
+
+## Bundling Assets
+By default, Laravel utilizes <a href="https://vitejs.dev/">Vite</a> to bundle your assets.
---
+++ routes/admin.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register admin routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::middleware(['auth', 'admin', 'verified'])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
